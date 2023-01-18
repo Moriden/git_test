af15491790f1b14fda6cbf22a2e3521a5cc83f75
kamamori <moradlouhbab@gmail.com>
2023-01-18 23:55:02 +0100

    Rock-Paper-Scissors

--- Rock-Paper-Scissors/javascript.js
+++ Rock-Paper-Scissors/javascript.js
@@ -5,3 +5,6 @@ function playRound (playerSelection, computerSelection) {
 let playerSelection = "rock";
 let computerSelection = "random()";
 console.log(playRound("playerSelector, computerSelection"));
+
+for (playRound=0; playRound<5; playRound++);
+console.log("playRound");
