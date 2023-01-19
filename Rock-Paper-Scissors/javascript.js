f0750dc96f5497d8f0c2c2ea2d0412e567c871ff
kamamori <moradlouhbab@gmail.com>
2023-01-19 19:10:37 +0100

    Rock-Paper-Scissors

--- Rock-Paper-Scissors/javascript.js
+++ Rock-Paper-Scissors/javascript.js
@@ -1,7 +1,51 @@
-function playRound (playerSelection, computerSelection) {
-    return playerSelection, computerSelection
+const userChoice = document.getElementsByClassName("userchoice");
+const computerChoice = document.getElementsByClassName("computerchoice");
+const resultDisplay = document.getElementsByClassName("result");
+const possibleChoices = document.querySelectorAll("button");
+
+let user;
+let computer;
+let result;  
+ 
+possibleChoices.forEach(button => button.addEventListener("click", () => {
+
+user = button.textContent;
+computerTurn();
+userChoice.textContent = `user" ${user}`;
+computerChoice.textContent = `computer ${computer}`;
+result.textContent = checkWinner();
+}));
+s
+function computerTurn(){
+    const randNum = Math.floor(Math.random() *3);
+
+    switch(randNum){
+        case 1:
+        computer = "ROCK";
+        break;
+        case 2:
+        computer = "PAPER";
+        break;
+        case 3:
+        computer = "SCISSORS";
+        break;
+    }
 }
 
-let playerSelection = "rock";
-let computerSelection = "random()";
-console.log(playRound("playerSelector, computerSelection"));
+function checkWinner() {
+    if (user == computer){
+return "Draw";
+}
+
+else if (computer == "ROCK") {
+return (user == "PAPER") ? "You win" : "You lose";
+}
+
+else if (computer == "PAPER"){
+    return (user == "SCISSORS") ? "You win" : "You lose";
+}
+
+else if (computer == "SCISSORS"){
+    return (user == "ROCK") ? "You win" : "You lose";
+}
+};
