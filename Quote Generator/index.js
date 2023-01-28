42fc4ad943af4f2bb18f3cf9e7fc55656ae79de9
kamamori <moradlouhbab@gmail.com>
2023-01-28 16:05:28 +0100

    Quote Generator

--- Quote Generator/index.js
+++ Quote Generator/index.js
@@ -1,1 +1,6 @@
+//variables
+
+let quote = document.querySelector(".text-area");
+let author = document.querySelector(".author");
+let btn = document.querySelector(".new-quote");
 
--- Rock-Paper-Scissors/javascript.js
+++ Rock-Paper-Scissors/javascript.js
@@ -1,56 +1,5 @@
-const playerChoice = document.querySelector(".playerChoice");
-const computerChoice = document.querySelector(".computerChoice");
-const resultEnd = document.querySelector(".resultEnd");
-const choiceBtn = document.querySelectorAll(".choiceBtn");
-
-let player;
-let computer;
-let result;
-
-choiceBtn.forEach(button => button.addEventListener ("click", () =>  {
-player = button.textContent;    
-computerTurn();
-
-playerChoice.textContent = `player ${player}`;
-computerChoice.textContent = `computer ${computer}`;
-resultEnd.textContent = checkWinner ();
-})); 
-
-
-function computerTurn() { 
-const randNum = Math.floor(Math.random() *3) +1;   
-
-switch(randNum){
-    case 1:
-    computer= "ROCK";
-    break;
-case 2:
-    computer= "PAPER";
-    break;
-case 3:
-        computer= "SCISSORS";
-    break;
+function getComputerChoice () {
+    const randomChoice = ("rock, paper, scissors");
+console.log (getComputerChoice());
 }
-}
-
-function checkWinner() {
-    if (player == computer){
-        return "Draw";
-    }
-
-    else if (computer == "ROCK") {
-        return (player == "PAPER") ? "You win" : "You lose"
-    }
-
-    else if (computer == "PAPER") {
-        return (player == "SCISSORS") ? "You win" : "You lose"
-    }
-
-    else if (computer == "SCISSORS") {
-        return (player == "ROCK") ? "You win" : "You lose"
-    }
-}
-
-
-
 
