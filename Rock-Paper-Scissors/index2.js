994ada76e1714a997ab48129b3d8a0eee61fe17f
kamamori <moradlouhbab@gmail.com>
2023-03-12 15:49:45 +0100

    Rock-Paper-Scissors

--- Rock-Paper-Scissors/index2.js
+++ Rock-Paper-Scissors/index2.js
@@ -32,26 +32,49 @@ function score () {
 };
 
 
-rockBtn.addEventListener('click', () => {
-    playRound(rockBtn);
-
-        const paperBtn = document.querySelector('.paperBtn');
-
-        paperBtn.addEventListener('click', () => {
-            playRound(paperBtn);
-    
-            const scissorsBtn = document.querySelector('.scissorsBtn');
-
-        scissorsBtn.addEventListener('click', () => {
-            playRound(scissorsBtn);
-            
-            const resultsDiv = document.querySelector('#resultEnd');
-
-            function playRound(playerSelection) {
-              // ...
-              resultsDiv.textContent = `You chose ${playerSelection}. Computer chose ${computerSelection}. ${result}`;
-            }
+function gameSettings (playerChoice, computerChoice) {
+    for (let round = 0; round < 5; round++) {
+        const playerChoice = prompt ('choose', 'rock', 'paper', 'scissors');
+        const computerChoice = computerPick();
+        const result = playRound(computerChoice, playerChoice);
+        console.log("ME",playerChoice, "COMPUTER",computerChoice, '=', result);
+    }
+}
 
 console.log(gameSettings());
 console.log(score());  
+
+
+
+const rockBtn = document.querySelector('.rockBtn');
+
+rockBtn.addEventListener('click', () => {
+    playRound(rockBtn);
+    console.log(rock);
+});
+
+const paperBtn = document.querySelector('.paperBtn');
+
+paperBtn.addEventListener('click', () => {
+playRound(paperBtn);
+console.log(paper);
+});
+
+const scissorsBtn = document.querySelector('.scissorsBtn');
+
+scissorsBtn.addEventListener('click', () => {
+playRound(scissorsBtn);
+console.log(scissors);
+});
+            
+const resultsDiv = document.querySelector('#resultEnd');
+function playRound(playerSelection) {
+resultsDiv.textContent = `You chose ${playerSelection}, Computer chose ${computerSelection}, ${result}`;
+}
+      
+
+const scoreDiv = document.querySelector('#score');
+const winnerDiv = document.querySelector('#winner');
+let playerScore = 0;
+let computerScore = 0;
 
